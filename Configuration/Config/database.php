9f6e5a765c0abc50fa665dc5fbbf3fd8ab0a556b
APPLE4869 <apple.4869.s@gmail.com>
2017-07-17 04:52:25 +0900

    edit db

--- Configuration/Config/database.php
+++ Configuration/Config/database.php
@@ -1,9 +1,9 @@
 <?php
 	
-	define('DB_HOST', 'mysql120.phy.lolipop.lan');
-	define('DB_NAME', 'LAA0782807-fudousan');
-	define('DB_USER', 'LAA0782807');
-	define('DB_PASSWORD', 'shokei4869');
+	define('DB_HOST', 'localhost');
+	define('DB_NAME', 'fudousan');
+	define('DB_USER', 'dbuser');
+	define('DB_PASSWORD', 'shokei');
 
 	try {
 		$dbh = new PDO('mysql:host=' . DB_HOST . ';charset=utf8;dbname=' . DB_NAME, DB_USER, DB_PASSWORD);
